1078ebbf70d5e687c4fb0ca32ec4735f5a6ed891
Qazi-Talha-Ali-00 <b22cs087@iitj.ac.in>
2025-02-12 05:12:00 +0530

    some more problems

---
+++ solutions/Sorting and Searching/reading_books.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    vector<long long int> arr(n);
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    sort(arr.begin(),arr.end());
+    int i,j;
+    i = 0,j=n-1;
+
+    return 0;
+}
